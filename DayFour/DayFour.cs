50d2cfa74a1f82cca31554b67103f3df5674bc83
Nightwine <nightwine@gmail.com>
2022-12-04 11:11:46 +0000

    Day four, adding analysing of work assignments

---
+++ DayFour/DayFour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022.DayFour {
+    public class DayFour {
+        private readonly List<WorkPair> _elfPairs;
+
+        public DayFour(string sectionAssignmentList) {
+            _elfPairs = new List<WorkPair>();
+            foreach (var pairAssignment in sectionAssignmentList.Split(new[] { Environment.NewLine }, StringSplitOptions.None)) {
+                _elfPairs.Add(new WorkPair(pairAssignment));
+            }
+        }
+
+        public int DuplicatedEffortCount() {
+            return _elfPairs.Count(x => x.FullyContainedPair() == true);
+        }
+
+        public int OverlappingEffortCount() {
+            return _elfPairs.Count(x => x.OverlappingPair() == true);
+        }
+    }
+}
---
+++ DayFour/SectionAssignment.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode2022.DayFour {
+    public class SectionAssignment {
+        private readonly int _minRange;
+        private readonly int _maxRange;
+
+        public SectionAssignment(string sectionAssignment) {
+            var range = sectionAssignment.Split('-');
+            _minRange = int.Parse(range[0]);
+            _maxRange = int.Parse(range[1]);
+        }
+
+
+        public bool ContainsSection(SectionAssignment otherSection) {
+            return _minRange <= otherSection._minRange && _maxRange >= otherSection._maxRange;
+        }
+
+        public bool OverlapsWithSection(SectionAssignment otherSection) {
+            return !(_maxRange < otherSection.MinRange() || otherSection.MaxRange() < _minRange);
+        }
+        
+        protected int MinRange() { return _minRange; }
+        protected int MaxRange() { return _maxRange; }
+    }
+}
