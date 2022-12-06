3fec44818db10f6a9ad5729311d269d64683160e
Nightwine <nightwine@gmail.com>
2022-12-06 17:34:11 +0000

    Another test

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Foo");
+            Console.WriteLine("Foo2");
         }
     }
 }
