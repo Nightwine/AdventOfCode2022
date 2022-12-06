c0201e3427dde26ec21b0ac35dd0e6a5f977c391
Nightwine <nightwine@gmail.com>
2022-12-06 19:52:57 +0000

    Day 6 message markers found

---
+++ Advent2022Tests/Day6Tests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2022.Day6;
+using NUnit.Framework;
+
+namespace Advent2022Tests {
+    public class Day6Tests {
+        
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+        public void ThenTheCharactersToStartOfPacketReturnsCorrectly(string input, int expectedResult) {
+            var day = new DaySix(input);
+            Assert.That(day.DoFirstPuzzle(), Is.EqualTo(expectedResult));
+        }
+
+        [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+        [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+        [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+        [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+        [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+        public void ThenTheCharactersToStartOfMessageReturnsCorrectly(string input, int expectedResult) {
+            var day = new DaySix(input);
+            Assert.That(day.DoSecondPuzzle(), Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ AdventOfCode2022/Day6/DaySix.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2022.Day6 {
+    public class DaySix {
+        private readonly PacketReader _packetReader;
+
+        public DaySix(string input) {
+            _packetReader = new PacketReader(input);
+        }
+
+        public int DoFirstPuzzle() {
+            return _packetReader.StartMarkerCompletedIndex();
+        }
+
+        public int DoSecondPuzzle() {
+            return _packetReader.StartMessageCompletedIndex();
+        }
+    }
+}
