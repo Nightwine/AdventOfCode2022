9e6f52ca122b0e82eab82b97b63d188b8b741106
Nightwine <nightwine@gmail.com>
2022-12-12 20:38:25 +0000

    And then all cleanliness left as she tried to catch up

---
+++ Advent2022Tests/Day8Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode2022.Day8;
+using NUnit.Framework;
+
+namespace Advent2022Tests {
+    public class Day8Tests {
+
+        [Test]
+        public void ThenTheVisibleTreesAreExpected() {
+            var trees = new TreeGrid(TestData());
+            Assert.That(trees.VisibleTrees(), Is.EqualTo(21));
+        }
+
+        [Test]
+        public void ThenTheScenicScoreIsExpected() {
+            var trees = new TreeGrid(TestData());
+            Assert.That(trees.ScenicScore(), Is.EqualTo(8));
+        }
+
+        private string TestData() {
+            return "30373\r\n25512\r\n65332\r\n33549\r\n35390";
+        }
+    }
+}
